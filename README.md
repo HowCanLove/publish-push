539fab77bfa9f55f92aa3ecb71da37ea6d49edab
liukai <604376264@qq.com>
2022-08-26 09:45:43 +0800

    初次提交

---
+++ README.md
@@ -0,0 +1,9 @@
+## 使用说明
+
+简化 npm publish，并自动提交到 git，可以选择是否将 更新内容同步到 readme
+
+```bash
+npm i npm-publish -g
+```
+
+在 npm 项目中执行 npm-publish
---
+++ bin/publishPub.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+import { readFileSync, writeFileSync, appendFileSync, readdirSync } from "fs";
+import moment from "moment";
+import shell from "shelljs";
+import inquirer from "inquirer";
+
+const { exec } = shell;
+const promptList = [
+  {
+    type: "input",
+    message: "更新内容:",
+    name: "updateCommit",
+  },
+  {
+    type: "confirm",
+    message: "将更新内容同步到readme.md:",
+    name: "addToReadme",
+  },
+];
+
+async function pub() {
+  const currentDir = exec("pwd").trim();
+  const defaultReadmePath = `${currentDir}/readme.md`;
+  const currentFiles = readdirSync(currentDir);
+
+  const packageFile = currentFiles.find((i) => /package.json/i.test(i));
+  const packagePath = packageFile ? `${currentDir}/${packageFile}` : "";
+
+  const readmeFile = currentFiles.find((i) => /readme.md/i.test(i));
+  let readmePath = readmeFile ? `${currentDir}/${readmeFile}` : "";
+
+  const packageResult = readFileSync(packagePath, "utf-8");
+  const versionLineMatch = packageResult.match(/version[^,]*/);
+  if (Array.isArray(versionLineMatch)) {
+    const versionLine = versionLineMatch[0];
+    const nextVersion = +versionLine.match(/.d*"$/)[0].replace(/\D/g, "") + 1;
+    const newVersionLine = versionLine.replace(/.d*"$/, `${nextVersion}"`);
+    const newPackage = packageResult.replace(versionLine, newVersionLine);
+
+    const { updateCommit, addToReadme } = await inquirer.prompt(promptList);
+    if (updateCommit && addToReadme) {
+      if (!readmePath) {
+        readmePath = defaultReadmePath;
+      }
+      const readmeFileContent = readFileSync(readmePath, "utf-8");
+
+      if (!readmeFileContent.includes("## 更新备注")) {
+        appendFileSync(readmePath, "\n## 更新备注\n  \n");
+      }
+
+      appendFileSync(
+        readmePath,
+        `${newVersionLine.replace(/[^\d^.]/g, "")} ---------- ${moment().format(
+          "YYYY-MM-DD HH:mm:ss"
+        )}  \n`
+      );
+      appendFileSync(readmePath, `更新内容: ${updateCommit}  \n`);
+    }
+    writeFileSync(packagePath, newPackage);
+
+    exec("npm publish");
+    exec("git status");
+    exec("git add .");
+    exec(`git commit -m "${updateCommit || "update"}"`);
+    exec(`git push origin master`);
+  }
+}
+pub();
